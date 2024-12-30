d2467300e66893486e6231ee62e9188fb66147ca
m4tice <ductuan2908@gmail.com>
2024-12-31 01:13:48 +0700

    update: rq1 colors

--- app/development/static/rq1.js
+++ app/development/static/rq1.js
@@ -1,12 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
     const table = document.getElementById("data-table");
-    const rows = table.getElementsByClassName("state"); 
+    const states = table.getElementsByClassName("state"); 
+    const doors = table.getElementsByClassName("DOORS");
     
-    for (let i = 0; i < rows.length; i++){
-        if ("Conflicted" === rows[i].innerText) {
-            rows[i].style.backgroundColor = "#FF9AA2";
-            rows[i].style.color = "#FFFFFF";
-            rows[i].style.fontWeight = "bold";
+    // LCS check
+    for (let i = 0; i < states.length; i++){
+        const stateCell = states[i];
+
+        if ("Conflicted" === stateCell.innerText) {
+            stateCell.style.backgroundColor = "#FFC000";
+            stateCell.style.fontWeight = "bold";
+        }
+
+        else if ("Evaluated" === stateCell.innerText) {
+            stateCell.style.backgroundColor = "#FFE699";
+            stateCell.style.fontWeight = "bold";
+        }
+    }
+
+    // DOORS check
+    for (let i = 0; i < doors.length; i++){
+        const doorCell = doors[i];
+
+        if ("missing" === doorCell.innerText) {
+            doorCell.style.backgroundColor = "#FF7C80";
+            doorCell.style.color = "white";
+            doorCell.style.fontWeight = "bold";
         }
     }
 });
