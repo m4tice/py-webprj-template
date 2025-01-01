8d38967a79bc4100de863ec9c369ce87b6bb1b43
m4tice <ductuan2908@gmail.com>
2025-01-01 21:59:08 +0700

    update: rq1 validation and buttons

--- app/development/static/rq1.js
+++ app/development/static/rq1.js
@@ -1,31 +1,45 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const table = document.getElementById("data-table");
+    const table = document.getElementById("rq1-table");
     const states = table.getElementsByClassName("state"); 
     const doors = table.getElementsByClassName("DOORS");
-    
-    // LCS check
-    for (let i = 0; i < states.length; i++){
-        const stateCell = states[i];
+    const rows = table.getElementsByTagName("tr");
 
-        if ("Conflicted" === stateCell.innerText) {
-            stateCell.style.backgroundColor = "#FFC000";
-            stateCell.style.fontWeight = "bold";
+    for (let i = 1; i < rows.length; i++){
+        const row = rows[i];
+
+        // State of element
+        const lifeCycleState = row.getElementsByTagName("td")[2];
+        
+        // allocation
+        const allocation = row.getElementsByTagName("td")[8];
+
+        // category
+        const category = row.getElementsByTagName("td")[9];
+
+        // DOORS
+        const doors = row.getElementsByTagName("td")[10];
+
+        // Life Cycle State validation
+        if ("Conflicted" === lifeCycleState.innerText) {
+            lifeCycleState.style.backgroundColor = "#FFC000";
+            lifeCycleState.style.fontWeight = "bold";
         }
 
-        else if ("Evaluated" === stateCell.innerText) {
-            stateCell.style.backgroundColor = "#FFE699";
-            stateCell.style.fontWeight = "bold";
+        else if ("Evaluated" === lifeCycleState.innerText) {
+            lifeCycleState.style.backgroundColor = "#FFE699";
+            lifeCycleState.style.fontWeight = "bold";
         }
-    }
 
-    // DOORS check
-    for (let i = 0; i < doors.length; i++){
-        const doorCell = doors[i];
+        // Allocation, Category and DOORS validation
+        if ("None" === allocation.innerText) {
+            allocation.style.backgroundColor = "#FFC000";
+            allocation.style.fontWeight = "bold";
+        }
 
-        if ("missing" === doorCell.innerText) {
-            doorCell.style.backgroundColor = "#FF7C80";
-            doorCell.style.color = "white";
-            doorCell.style.fontWeight = "bold";
+        if (("Software" === allocation.innerText) && ("None" === doors.innerText)) {
+            doors.style.backgroundColor = "#FF7C80";
+            doors.style.color = "white";
+            doors.style.fontWeight = "bold";
         }
     }
 });
